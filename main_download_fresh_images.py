93062c34781843fbaaa787d55c97028246a99324
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-03-13 14:10:44 +0300

    изменил os.path.dirname(os.path.abspath(__file__)) и добавил отдельную функцию для сравнения данных с результатом в текстовом файле

--- main_download_fresh_images.py
+++ main_download_fresh_images.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from pathlib import Path
 from struct import error
 
@@ -9,17 +8,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+from compare_with_saved_data import CompareWithSavedData
 from tools.clear_folder_from_old_files import delete_old_folder
 from tools.crome_options import setting_chrome_options
 from tools.exif_job import write_tags_to_image
 from tools.image_downloader import downloader
 from tools.send_message_to_telegram import send_telegram_message
-from tools.write_album_name_to_file import save_album_name, read_albums
+from tools.write_album_name_to_file import save_album_name
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=setting_chrome_options())
 
-script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+script_dir = os.path.dirname(os.path.abspath(__file__))
 log_file_path = os.path.join(script_dir, "ska_downloader_log.log")
 logger.add(log_file_path, format="{time} {level} {message}", level="INFO", retention="1 day", rotation="1 day")
 
@@ -58,22 +58,29 @@ if __name__ == '__main__':
     _last_album_url = last_album(_all_albums)
     logger.info(f'{_last_album_url = }')
 
-    # нужно получить ссылки на все снимки в альбоме
-    images_links_list = all_images(_last_album_url)[0]
-    logger.info(f'{len(images_links_list) = }')
-
     # получаю название альбома
     _album_name = all_images(_last_album_url)[1]
-    Path(f'{script_dir}/images/{_album_name}').mkdir(parents=True, exist_ok=True)
-    path_to_download = Path(f'{script_dir}/images/{_album_name}')
-    logger.info(path_to_download)
     logger.info(_album_name)
 
+    with open(f'{script_dir}/albums.txt', 'w', encoding='UTF8') as text_file:
+        text_file.write('')
+
     # проверяю не скачан ли альбом ранее
-    old_albums = read_albums(script_dir)
+
+
+    if CompareWithSavedData(script_dir, 'albums.txt', _album_name).compare():
+    # old_albums = read_albums(script_dir)
     # logger.info(old_albums = read_albums())
 
-    if _album_name + '\n' not in old_albums:
+    # if _album_name + '\n' not in old_albums:
+
+        # нужно получить ссылки на все снимки в альбоме
+        images_links_list = all_images(_last_album_url)[0]
+        logger.info(f'{len(images_links_list) = }')
+
+        Path(f'{script_dir}/images/{_album_name}').mkdir(parents=True, exist_ok=True)
+        path_to_download = Path(f'{script_dir}/images/{_album_name}')
+        logger.info(path_to_download)
 
         save_album_name(_album_name, script_dir)
         send_telegram_message(_album_name)
